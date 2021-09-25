3e21174557b6845483b710f27abd0707336d652b
Nagababu Thota <nagababuthota593@gmail.com>
2021-09-25 12:16:34 +0530

    All Five methods are working.

--- BankingApplication/Account.cs
+++ BankingApplication/Account.cs
@@ -13,11 +13,16 @@ namespace BankingApplication
         public string accountType;
         public double balance;
         public const int MIN_BALANCE = 250;
+        public static Dictionary<double, Dictionary<string, string>> accounts;
+        public static Dictionary<double, string> transactions;
         public Account()
         {
             double accountNumber = 0;
             string accountType = "";
             double balance = 0;
+            accounts = readAccounts();
+            transactions = readTransactions();
+
 
         }
         public void createAccount()
@@ -56,7 +61,7 @@ namespace BankingApplication
             Console.WriteLine(this.accountNumber);
 
 
-            Dictionary<double,Dictionary<string,string>> accounts = readAccounts();
+            //Dictionary<double,Dictionary<string,string>> accounts = readAccounts();
             
             if(!accounts.ContainsKey(this.accountNumber))
             { 
@@ -86,8 +91,10 @@ namespace BankingApplication
         {
 
             Dictionary<double, Dictionary<string,string>> jsonObject = new Dictionary<double, Dictionary<string,string>>();
-            string data = File.ReadAllText("accounts.json");
+
+            string data = File.ReadAllText("C:\\Users\\nagab\\OneDrive\\Desktop\\Technovert\\Banking Application\\BankingApplication\\accounts.json");
             jsonObject = JsonConvert.DeserializeObject<Dictionary<double,Dictionary<string,string>>>(data);
+           
             return jsonObject;
             
 
@@ -95,72 +102,232 @@ namespace BankingApplication
         public static void writeAccounts(Dictionary<double, Dictionary<string, string>> dataToWrite)    //writes to json file
         {
             string serializedData = JsonConvert.SerializeObject(dataToWrite,Formatting.Indented);
-            File.WriteAllText("accounts.json",serializedData);
-            //data not being written :(
+            File.WriteAllText("C:\\Users\\nagab\\OneDrive\\Desktop\\Technovert\\Banking Application\\BankingApplication\\accounts.json", serializedData);
+            //data written into json.
         }
+        public static Dictionary<double,string> readTransactions()
+        {
+            string data = File.ReadAllText("C:\\Users\\nagab\\OneDrive\\Desktop\\Technovert\\Banking Application\\BankingApplication\\transactions.json");
+            return JsonConvert.DeserializeObject<Dictionary<double, string>>(data);
+        }
+        public static void writeTransactions(Dictionary<double,string> transactions)
+        {
+            string serializedData = JsonConvert.SerializeObject(transactions, Formatting.Indented);
+            File.WriteAllText("C:\\Users\\nagab\\OneDrive\\Desktop\\Technovert\\Banking Application\\BankingApplication\\transactions.json", serializedData);
+        }
+
         public void depositAmount()
         {
             //deposits money and updates account details.
-            //int amount;
-            //double accNumber;
-            //Console.WriteLine("Enter your Account Number: ")
-            //accNumber = Convert.ToInt64(Console.WriteLine());
-            //if(accNumber == 1)   //check for validity.
-            //{
-            //    Console.WriteLine("Enter amount to deposit: ");
-            //    amount = Convert.ToInt32(Console.WriteLine);
-            //    if(amount>0)
-            //    {
-            //          //update current object's available balance.
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("Invalid amount to deposit.");
-            //        return;
-            //    }
-            //}
+            int amount;
+            double accNumber;
+            Console.WriteLine("Enter your Account Number: ");
+            accNumber = Convert.ToDouble(Console.ReadLine());
+            
+            if (accounts.ContainsKey(accNumber))   //check for validity.
+            {
+                Console.WriteLine("Hello! {0}",accounts[accNumber]["name"]);
+                Console.WriteLine("Enter amount to deposit: ");
+                amount = Convert.ToInt32(Console.ReadLine());
+                if (amount > 0)
+                {
+                    //update current object's available balance.
+                    string details = DateTime.Now + " " + amount+"INR" + " Credited";
+                    amount = Convert.ToInt32(accounts[accNumber]["balance"]) + amount;
+                    accounts[accNumber]["balance"] = Convert.ToString(amount);
+                    writeAccounts(accounts);
+                    Console.WriteLine("Amount has been credited successfully!");
+                    Console.WriteLine("Current balance ->{0}\n\n", accounts[accNumber]["balance"]);
+
+                    //making the transaction
+                    if (!transactions.ContainsKey(accNumber))
+                    {
+                        transactions.Add(accNumber, details);
+                    }
+                    else
+                    {
+                        transactions[accNumber] += "," + details;
+                    }
+                    writeTransactions(transactions);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid amount to deposit.");
+                    return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Account doesn't exist!");
+            }
         }
         public void withdrawAmount()
         {
             //withdraws money and updates account details.
-            //int amount = 0;
-            //Console.WriteLine("Enter amount to withdraw: ");
-            //amount = Convert.ToInt32(Console.ReadLine());
-            //if(amount>0)
-            //{
-            //    //do
-            //}
-            //else if (amount > Account.TRANS_LIMIT)
-            //{
-            //    Console.WriteLine("Couldn't perform transaction. Transaction limit reached.\n")
-            //}
-            //else if(amount>Account.DAY_LIMIT)
-            //{
-            //    Console.WriteLine("Couldn't perform transaction. Daylimit reached!\n");
-            //    return;
-            //}
-            
+            int amount = 0;
+            double accNumber;
+            Console.WriteLine("Enter your Account Number: ");
+            accNumber = Convert.ToDouble(Console.ReadLine());
+
+            if (accounts.ContainsKey(accNumber))   //check for validity.
+            {
+                Console.WriteLine("Hello! {0}", accounts[accNumber]["name"]);
+                Console.WriteLine("Enter amount to withdraw: ");
+                amount = Convert.ToInt32(Console.ReadLine());
+                if (amount > 0)
+                {
+                    if (amount <= Convert.ToInt32(accounts[accNumber]["balance"]))
+                    {
+                        //update current object's available balance.
+                        string details = DateTime.Now + " " + amount + "INR" + " Dedited";
+                        amount = Convert.ToInt32(accounts[accNumber]["balance"]) - amount;
+                        accounts[accNumber]["balance"] = Convert.ToString(amount);
+                        writeAccounts(accounts);
+                        Console.WriteLine("Amount has been debited successfully!");
+                        Console.WriteLine("Current balance ->{0}\n\n", accounts[accNumber]["balance"]);
+                        //making the transaction
+                        if (!transactions.ContainsKey(accNumber))
+                        {
+                            transactions.Add(accNumber, details);
+                        }
+                        else
+                        {
+                            transactions[accNumber] += "," + details;
+                        }
+                        writeTransactions(transactions);
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Insufficient account balance.\n");
+                    }
+
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid amount to withdraw.\n");
+                    return;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Account doesn't exist.");
+                return;
+            }
         }
         public void transferAmount()
         {
             //transfers money from one accc to another
-            //double senderAcc;
-            //double receiverAcc;
-            //int amount;
-            //Console.WriteLine("Enter Sender's account number: ");
-            //senderAcc = Convert.ToInt64(Console.ReadLine());
-            //Console.WriteLine("Enter Receiver's account number: ");
-            //receiverAcc = Convert.ToInt64(Console.ReadLine());
-            //if(senderAcc ==1 && receiverAcc ==1)//check whether both of the accounts exist.
-            //{
-            //    Console.WriteLine("Enter amount to Transfer: ");
+            double senderAcc;
+            double receiverAcc;
+            int amount;
+            Console.WriteLine("Enter Sender's account number: ");
+            senderAcc = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Receiver's account number: ");
+            receiverAcc = Convert.ToDouble(Console.ReadLine());
+            if (accounts.ContainsKey(senderAcc))//check whether both of the accounts exist.
+            {
+                if (accounts.ContainsKey(receiverAcc))
+                {
+                    Console.WriteLine("Enter amount to Transfer: ");
+                    amount = Convert.ToInt32(Console.ReadLine());
+                    if (amount > 0)
+                    {
+                        if (amount <= Convert.ToInt32(accounts[senderAcc]["balance"]))
+                        {
+                            //generate transaction details for both sender and receiver.
+                            string receiverDetails = DateTime.Now + " " + amount + "INR" + " Transfered from " + accounts[senderAcc]["name"];
+                            string senderDetails = DateTime.Now + " " + amount + "INR" + " Transfered to " + accounts[receiverAcc]["name"];
+                                                                                                            
+                            int senderBalance = Convert.ToInt32(accounts[senderAcc]["balance"]) - amount;   //calculate sender and receiver balances
+                            int receiverBalance = Convert.ToInt32(accounts[receiverAcc]["balance"]) + amount;
 
-            //}
+                            accounts[senderAcc]["balance"] = Convert.ToString(senderBalance);            //update sender and receiver balances.
+                            accounts[receiverAcc]["balance"] = Convert.ToString(receiverBalance);
 
+                            writeAccounts(accounts);
+
+                            Console.WriteLine("Amount has been transferred to {0} successfully!", accounts[receiverAcc]["name"]);
+                            Console.WriteLine("Current balance ->{0}\n\n", accounts[senderAcc]["balance"]);
+
+
+                            //lgging the transaction for sender
+                            if (!transactions.ContainsKey(senderAcc))
+                            {
+                                transactions.Add(senderAcc, senderDetails);
+                            }
+                            else
+                            {
+                                transactions[senderAcc] += "," + senderDetails;
+                            }
+                            //logging the transaction for receiver.
+                            if (!transactions.ContainsKey(receiverAcc))
+                            {
+                                transactions.Add(receiverAcc, receiverDetails);
+                            }
+                            else
+                            {
+                                transactions[receiverAcc] += "," + receiverDetails;
+                            }
+
+
+                            writeTransactions(transactions);
+
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Insufficient account balance.\n");
+                        }
+
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid receipient account number.");
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid sender account number.");
+                }
+
+            }
         }
         public void printTransactionHistory()
         {
             //prints user's acount history
+            Console.WriteLine("Enter your account number: ");
+            double accNumber = Convert.ToDouble(Console.ReadLine());
+            if(accounts.ContainsKey(accNumber))
+            {
+                if(transactions.ContainsKey(accNumber))
+                {
+                    string trans = transactions[accNumber];
+                    string[] transList = trans.Split(",");
+                    int count = 1;
+                    foreach(string transaction in transList)
+                    {
+                        Console.WriteLine("{0}\t{1}",count,transaction);
+                        count++;
+                    }
+                    Console.WriteLine("\n");
+                }
+                else
+                {
+                    Console.WriteLine("None transactions recorded so far!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Account doesn't exist.");
+                return;
+            }
+
         }
     }
 }
--- BankingApplication/Program.cs
+++ BankingApplication/Program.cs
@@ -17,24 +17,30 @@ namespace BankingApplication
 
                 Console.WriteLine("Choose any one option:\n1.Create Account\n2.Deposit\n3.Withdraw\n4.Transfer Amount\n5.Print Transaction history\n");
                 int choice = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine(choice);
                 Account acc = new Account();
                 switch (choice)
                 {
                    
-                    case 1: Console.WriteLine("Account to be Created!\n");
+                    case 1:
+                            Console.WriteLine("\t-------Account Creation-------\n");
                             acc.createAccount();
                             break;
-                    case 2: Console.WriteLine("Amount deposited\n");
+                    case 2:
+                            Console.WriteLine("\t-------Money Deposit-------\n");
+                            acc.depositAmount();
                             break;
-                    case 3: Console.WriteLine("Amount Withdrawl\n");
+                    case 3: Console.WriteLine("\n-------Amount Withdrawl-------\n");
+                            acc.withdrawAmount();
                             break;
-                    case 4: Console.WriteLine("Amount transfered\n");
+                    case 4: Console.WriteLine("-------Amount Transfer-------\n");
+                            acc.transferAmount();
                             break;
-                    case 5: Console.WriteLine("Transaction History\n");
+                    case 5: Console.WriteLine("-------Transaction History-------\n");
+                            acc.printTransactionHistory();
                             break;
                             
                     default:
+                        Environment.Exit(0);
                         break;
                 }
 
